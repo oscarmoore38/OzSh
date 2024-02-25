db794a07253979683b81ca75df8fc84d43d01db8
oscarmoore38 <oscarjmoore38@gmail.com>
2024-02-25 14:31:21 -0500

    Added code to create util object and validate inputs

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
-#include "shell.h"
+#include "shellClass/shell.h"
+#include "utilClass/util.h"
 
 
 int main(int argc, char* argv[]){
-    
+    // Validate inputs
+    Util shellUtil; 
 
-    Shell newShell; 
+    bool isValidInput = shellUtil.is_valid_input(argc, argv);
 
-    newShell.run(); 
+    if (isValidInput) {
+        std::cout << "Valid input - creating new shell" << std::endl;
+
+    } else {
+        std::cout << "invalid input - exiting" << std::endl;
+    }
+
+
+    // Create new shell
+    // Shell newShell; 
+
+    // newShell.run(); 
 
     return 0; 
 }
