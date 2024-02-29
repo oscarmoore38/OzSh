f6c14d5c0a1642fe123ed7cd72a9ad44eaaca760
oscarmoore38 <oscarjmoore38@gmail.com>
2024-02-29 11:02:27 -0500

    Added input validation function

--- src/shellClass/shell.cpp
+++ src/shellClass/shell.cpp
@@ -1,6 +1,25 @@
 #include "shell.hpp"
+#include "../utilClass/util.hpp"
+#include "../shellExceptionClass/shellException.hpp"
+
+using namespace std;
 
 // constructor definiton 
 Shell:: Shell(){
 
 }
+
+void Shell:: is_valid_input(int argumentCount, char* argumentValue[]){
+    try {
+        shellUtilityObject.is_valid_input(argumentCount, argumentValue);
+    } catch (const ShellException& e) {
+        cerr << e.what() << endl;
+        exit(1);
+    }
+}
+
+
+void Shell:: run(){
+
+}
+
--- src/shellClass/shell.hpp
+++ src/shellClass/shell.hpp
@@ -1,11 +1,12 @@
 #ifndef SHELL_HPP
 #define SHELL_HPP
 
+#include "../utilClass/util.hpp"
 // #include "CommandParser.h"
 // #include "CommandExecutor.h"
 // #include "ProcessManager.h"
 // #include "SignalHandler.h"
-// #include "UserInterface.h"
+
 
 /**
  * @class Shell
@@ -20,6 +21,7 @@ private:
     // ProcessManager ProcessManager;
     // SignalHandler SignalHandler; 
     // UserInterface UserInterface; 
+    Util shellUtilityObject;
 
 public: 
     // Constructor 
@@ -30,12 +32,25 @@ public:
      */
     Shell();
 
+    // Class Methods 
+    
+    /** 
+     * @brief Checks if a user input is valid. Will throw execption if not. 
+     *
+     * @param arguementCount (int) Total count of arguments passed to program by user.
+     * @param argumentValue (char * []) Array of values passed to program by user.
+     * 
+     * @return (bool) Bool value indicating if input is valid or not.
+     */
+    void is_valid_input(int argumentCount, char* argumentValue[]);
+
     /** 
      * @brief starts main loop for shell. 
      * 
      * @note This method takes no input parameters. 
      */
     void run();
+ 
 
 };
 
