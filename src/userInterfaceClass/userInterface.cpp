f17e0c4b4e2fccad043131fd4e6eb69d7031b3ea
oscarmoore38 <oscarjmoore38@gmail.com>
2024-03-04 21:39:04 -0500

    Added cwd and hostname to prompt

--- src/userInterfaceClass/userInterface.cpp
+++ src/userInterfaceClass/userInterface.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <unistd.h>
+#include <libgen.h>
+
 
 using namespace std;
 
 UserInterface:: UserInterface(){
-    // Check PS1 value and set if needed. 
+    // Check PS1 value and username and set if needed. 
     envVarPS1 = getenv("PS1");
     if (envVarPS1 == nullptr){
         setPromptEnv();
@@ -14,16 +16,40 @@ UserInterface:: UserInterface(){
 }
 
 void UserInterface:: printPrompt(){
-    cout << envVarPS1 << endl;
+    cout << envVarPS1;
 }
 
 
 void UserInterface:: setPromptEnv(){
-    // Root user 
+    // Get prompt
+    string prompt; 
     if (getuid() == 0){
-        setenv("PS1", "# ", 0);
+        prompt = " # ";
     } else {
-        setenv("PS1", "$ ", 0);
+        prompt = " $ ";
     }
+    
+    // Get the current hostname
+    char hostname[256]; 
+    if (gethostname(hostname, sizeof(hostname)) != 0) {
+        // If unable to retrieve the hostname, display a default value
+        strcpy(hostname, "unknown");
+    }
+
+    // Get the current working directory
+    char cwd[1024]; 
+    if (getcwd(cwd, sizeof(cwd)) == nullptr) {
+        // If unable to retrieve the working directory, display a default value
+        strcpy(cwd, "UNKNOWN");
+    } else {
+        // Extract the basename of the current working directory
+        char* cwd_basename = basename(cwd);
+        strcpy(cwd, cwd_basename);
+    }
+
+    // Set the PS1 environment variable
+    setenv("PS1", (string(hostname) + " " + string(cwd) + prompt).c_str(), 0);
+    
     envVarPS1 = getenv("PS1");
+
 }
