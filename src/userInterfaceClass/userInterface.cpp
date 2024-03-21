64b7f2b090abb9a7a71dd318d0c802e9350eaccb
oscarmoore38 <oscarjmoore38@gmail.com>
2024-03-21 09:42:28 -0400

    Moved away from using PS1 env variable for prompt so prompt can be dynamically updated after cd command

--- src/userInterfaceClass/userInterface.cpp
+++ src/userInterfaceClass/userInterface.cpp
@@ -9,47 +9,32 @@ using namespace std;
 
 UserInterface:: UserInterface(){
     // Check PS1 value and username and set if needed. 
-    envVarPS1 = getenv("PS1");
-    if (envVarPS1 == nullptr){
-        setPromptEnv();
-    }
+    setPromptEnv();
 }
 
 void UserInterface:: printPrompt(){
-    cout << envVarPS1;
+    std::cout << hostname << " " << cwd << prompt << " ";
 }
 
 
 void UserInterface:: setPromptEnv(){
-    // Get prompt
-    string prompt; 
+    // Get prompt 
     if (getuid() == 0){
         prompt = " # ";
     } else {
-        prompt = " $ ";
+        prompt = " % ";
     }
     
-    // Get the current hostname
-    char hostname[256]; 
     if (gethostname(hostname, sizeof(hostname)) != 0) {
-        // If unable to retrieve the hostname, display a default value
         strcpy(hostname, "unknown");
     }
 
     // Get the current working directory
-    char cwd[1024]; 
     if (getcwd(cwd, sizeof(cwd)) == nullptr) {
-        // If unable to retrieve the working directory, display a default value
         strcpy(cwd, "UNKNOWN");
     } else {
         // Extract the basename of the current working directory
         char* cwd_basename = basename(cwd);
         strcpy(cwd, cwd_basename);
     }
-
-    // Set the PS1 environment variable
-    setenv("PS1", (string(hostname) + " " + string(cwd) + prompt).c_str(), 0);
-    
-    envVarPS1 = getenv("PS1");
-
 }
--- src/userInterfaceClass/userInterface.hpp
+++ src/userInterfaceClass/userInterface.hpp
@@ -1,16 +1,23 @@
 #ifndef UI_HPP
 #define UI_HPP
+#include <iostream>
 
+using namespace std;
 
 /**
  * @class UserInterface
- * @brief Creates user interface for users to interact with program. 
+ * @brief Creates user interface for users to interact with program. I'm choosing
+ * not to use the PS1 environment variable because of the need to dynamically update
+ * working directory during shell session. 
  *
  */
 
 class UserInterface{
 private: 
     char* envVarPS1;
+    char cwd[1024]; 
+    char hostname[256];
+    string prompt;
 public: 
     // class method 
     // Constructor 
